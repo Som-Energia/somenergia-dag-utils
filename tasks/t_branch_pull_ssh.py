0a1092b0cd52400a59a6dfa99993c39ef8a17c3e
pol <pol.monso@somenergia.coop>
2022-11-02 16:25:29 +0100

    print dag id

--- tasks/t_branch_pull_ssh.py
+++ tasks/t_branch_pull_ssh.py
@@ -41,7 +41,7 @@ def pull_repo_ssh(repo_github_name, repo_server_url, repo_server_key, task_name,
     #Apareix quan fem molts git pull a la vegada
 
     # image removal and build is not working atm
-    print('dag id is {{ dag.dag_id}}')
+    print(f'dag id is {dag_id}')
     if dag_id and dag_id == 'dades_sandbox_dag':
         return 'update_docker_image' if requirements_updated else task_name
     else:
